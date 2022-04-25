bc3448cea4691621e7180cf5ef05f00856dcc105
Eunggseo <103013141+Eunggseo@users.noreply.github.com>
2022-04-24 23:12:29 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -17,11 +17,11 @@ public class MarkdownParse {
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
             if(openBracket == -1 || closeBracket==-1 || openParen == -1 || closeParen == -1){
-              break;
+              
             }
-            else if (=' ') {
-                
-            }
+           
+
+        
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
             
