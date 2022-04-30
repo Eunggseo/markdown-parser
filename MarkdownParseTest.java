c3558160edbe242d7b392ac400b457e2785bc445
Eunggseo <103013141+Eunggseo@users.noreply.github.com>
2022-04-30 11:36:44 -0700

    .

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,25 +1,24 @@
 import static org.junit.Assert.*;
 
-import java.beans.Transient;
+import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.*;
-public class MarkdownParseTest {
 
+public class MarkdownParseTest {
     @Test
-    public void addition() {
+    public void addition(){
         assertEquals(2, 1 + 1);
     }
 
-
-     @Test
-  public void testGetLinks() throws IOException{
-         Path fileName = Path.of("test-file.md");
-         String content = Files.readString(fileName);
-         ArrayList<String> links = MarkdownParse.getLinks(content);
-         assertEquals(List.of("https://something.com", "some-thing.html"),links);
-     }
-
-
+    @Test
+    public void testGetLinks() throws IOException{
+        Path fileName = Path.of("test-file.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+        assertEquals(List.of("https://something.com", "some-thing.html"), links);
+    }
 }
